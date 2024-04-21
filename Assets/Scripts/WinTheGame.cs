8cb8b4e8c073abaa56aa1daa8c9ef3914c4c0eb1
LouisVanhove <vanhovelouis123@gmail.com>
2024-04-22 00:40:41 +0200

    new burning asset, winscreen

--- Assets/Scripts/WinTheGame.cs
+++ Assets/Scripts/WinTheGame.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class WinTheGame : MonoBehaviour
 {
     [HideInInspector] public bool GameOverWin;
+    private Transform _winCanvas;
+
+    private void Start()
+    {
+        GameOverWin = false;
+        _winCanvas = GameObject.Find("WinCanvas").transform;
+        _winCanvas.GetChild(0).gameObject.SetActive(false); // deactivate image of win
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             GameOverWin = true;
+            _winCanvas.GetChild(0).gameObject.SetActive(true); // activate image of win
         }
     }
 
